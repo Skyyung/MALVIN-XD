d04468c5bf9ced58064c6d9272dafbeca3d30b82
Skyyung <skyyung55@outlook.com>
2025-08-25 03:20:17 -0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Nukw1ZRJ#M6M86pLiRC_dlHYzcd9DJjonHHLxMdSuAL51MekAKaQ",  // add your Session Id here
+  SESSION_ID: "malvin~JulHFYyJ#GlWl6dA4hmETTfQkPIjEx4QwbT4rRcVgxfBtERL5-gY",  // add your Session Id here
   
   OWNER_NUMBER: "2348070982236", // put your phone number here
   
